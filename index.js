2347822e08a10de9c7dda49c5089fa5fbb59ed98
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 15:36:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key]=value
   return object
 }
+function deleteFromObjectByKey(object, key){
+  var newObject = object
+  delete newObject[key]
+  return newObject
+}
