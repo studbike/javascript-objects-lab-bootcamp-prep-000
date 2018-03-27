e2dc44c8d2097f3454bb95b97293d3becf635033
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 15:49:55 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = {}
+var recipes = {chocolate:'bars',vanille:'wafers'}
 
 function updateObjectWithKeyAndValue(object, key, value){
   return Object.assign({}, object, { [key]: value })
